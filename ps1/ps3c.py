be69d65ca3356988ffe2c2f18194dd826376ffba
Joshmannb <57748350+Joshmannb@users.noreply.github.com>
2019-12-06 20:55:34 +0800

    ps1 done with bad style

--- ps1/ps3c.py
+++ ps1/ps3c.py
@@ -6,8 +6,8 @@
 # semi_annual_raise = float(input('Please enter your semi-annual salary raise percentage, as a decimal: '))
 annual_salary = float(input('Please enter your annual salary: '))
 def house_hunting(annual_salary, total_cost=1e6, semi_annual_raise=0.07):
-    upper_portion_saved = 1.
-    lower_portion_saved = 0.
+    upper_portion_saved = 10000
+    lower_portion_saved = 0
     count = 0
     target_month = 36
     portion_down_payment = 0.25 # 首付是房價的25%
@@ -16,31 +16,35 @@ def house_hunting(annual_salary, total_cost=1e6, semi_annual_raise=0.07):
     down_payment = total_cost * portion_down_payment
     # print(down_payment)
 
-    while not current_savings - 100 < down_payment < current_savings + 100:
+    while not down_payment - 100 < current_savings < down_payment + 100:
         month = 0
         current_savings = 0.
         start_salary = annual_salary
-        middle_portion_saved = (upper_portion_saved + lower_portion_saved) / 2
+        middle_portion_saved = int((upper_portion_saved + lower_portion_saved) / 2)
         count += 1
         while month != target_month:
             month += 1
             if month % 6 == 1 and month > 6:
                 start_salary += start_salary * semi_annual_raise
-            current_savings = current_savings + (start_salary / 12) * middle_portion_saved + current_savings * r
+            current_savings = current_savings + ((start_salary / 12) * middle_portion_saved) / 10000 + current_savings * r
             # print(current_savings - 100 > down_payment)
             # print(start_salary)
-            if current_savings - 100 > down_payment:
+            if current_savings > down_payment + 100:
                 upper_portion_saved = middle_portion_saved
                 # print(month)
                 break
-        if current_savings + 100 < down_payment:
+        if current_savings < down_payment - 100:
+            print(month)
             lower_portion_saved = middle_portion_saved
         # print(upper_portion_saved, lower_portion_saved)
-    return middle_portion_saved, count
+
+    return (middle_portion_saved) / 10000, count
 
 
 
 # print(house_hunting(portion_saved, total_cost, annual_salary))
 if __name__ == '__main__':
     print('''Best savings rate: {0:.4f}
-Steps in bisecion search: {1:}'''.format(house_hunting(annual_salary)[0], house_hunting(annual_salary)[1]))
+             Steps in bisecion search: {1:}'''.format(
+                 house_hunting(annual_salary)[0],
+                 house_hunting(annual_salary)[1]))
