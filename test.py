e7755d048809a058ea1f9119f9da8eba695952d2
Joshmannb <57748350+Joshmannb@users.noreply.github.com>
2020-01-08 00:22:16 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-a = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-print('a' in a)
+list = [2.9715, 1.1434, 0.7667, 0.5154]
+sum_eigen = sum(list)
+sum_ith = 0
+for i in range(len(list)):
+    sum_ith += list[i]
+    ratio = sum_ith/sum_eigen
+    print('sum to {0:d}th eigenvalues over sum of all eigenvalues:'.format(i+1), ratio)
+    
